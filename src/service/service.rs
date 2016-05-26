caed516a94061a7f921498e75cb617607ee84a1a
J/A <archer884@gmail.com>
2016-05-26 17:37:13 -0500

    Use client stored on Service
    Rather than creating a new client with each request to the API, this patch allows us to keep a single Hyper client as a field on the service and use it in our queries.

--- src/service/service.rs
+++ src/service/service.rs
@@ -7,18 +7,23 @@ static URI_BASE: &'static str = "http://quotes.rest";
 
 #[derive(Debug)]
 pub struct Service {
-    key: Option<String>
+    client: Client,
+    key: Option<String>,
 }
 
 // Struct method implementation
 impl Service {
     pub fn new() -> Service {
-        Service { key: None }
+        Service {
+            client: Client::new(),
+            key: None,
+        }
     }
 
     pub fn with_key<T: Into<String>>(key: T) -> Service {
         Service {
-            key: Some(key.into())
+            client: Client::new(),
+            key: Some(key.into()),
         }
     }
 
@@ -42,25 +47,22 @@ impl Default for Service {
 
 // Service method implementation
 impl Service {
-    // GET: http://quotes.rest/qod.json
     pub fn qod(&mut self) -> Result<Quote> {
-        Client::new().get(&self.build_uri(URI_BASE, "qod.json"))
+        self.client.get(&self.build_uri(URI_BASE, "qod.json"))
             .send()?
             .model::<QuoteResponse>()?
             .content()
     }
 
-    // GET: http://quotes.rest/qod/categories.json
     pub fn qod_categories(&mut self) -> Result<Categories> {
-        Client::new().get(&self.build_uri(URI_BASE, "qod/categories.json"))
+        self.client.get(&self.build_uri(URI_BASE, "qod/categories.json"))
             .send()?
             .model::<CategoryResponse>()?
             .content()
     }
 
-    // GET: http://quotes.rest/qod/qod.json?category=testing
     pub fn qod_for_category(&mut self, category: &str) -> Result<Quote> {
-        Client::new().get(&apply_query(
+        self.client.get(&apply_query(
             self.build_uri(URI_BASE, "qod.json"),
             &Query::new().with_category(category))
         ).send()?.model::<QuoteResponse>()?.content()
@@ -68,43 +70,39 @@ impl Service {
 
     // Everything below here requires an API key
 
-    // GET: http://quotes.rest/quote.json
     pub fn random(&mut self) -> Result<Quote> {
         if self.key.is_none() {
             return Err(Error::MethodUnavailable);
         }
 
-        Client::new().get(&self.build_uri(URI_BASE, "quote.json"))
+        self.client.get(&self.build_uri(URI_BASE, "quote.json"))
             .send()?
             .model::<QuoteResponse>()?
             .content()
     }
 
-    // GET: http://quotes.rest/quote.json?minlength=100&maxlength=300
     pub fn query(&mut self, query: &Query) -> Result<Quote> {
         if self.key.is_none() {
             return Err(Error::MethodUnavailable);
         }
 
-        Client::new().get(&apply_query(
+        self.client.get(&apply_query(
             self.build_uri(URI_BASE, "quote.json"),
             query,
         )).send()?.model::<QuoteResponse>()?.content()
     }
 
-    // GET: http://quotes.rest/quote/categories.json
     pub fn categories(&mut self) -> Result<Categories> {
         if self.key.is_none() {
             return Err(Error::MethodUnavailable);
         }
 
-        Client::new().get(&self.build_uri(URI_BASE, "quote/categories.json"))
+        self.client.get(&self.build_uri(URI_BASE, "quote/categories.json"))
             .send()?
             .model::<CategoryResponse>()?
             .content()
     }
 
-    // PUT: http://quotes.rest/quote.json?quote=<quote>&author=<author>&tags=<tags>
     pub fn add(&mut self) -> Result<()> {
         unimplemented!()
 
@@ -113,25 +111,23 @@ impl Service {
         // }
     }
 
-    // GET: http://quotes.rest/quote/image.json
     pub fn image(&mut self) -> Result<Image> {
         if self.key.is_none() {
             return Err(Error::MethodUnavailable);
         }
 
-        Client::new().get(&self.build_uri(URI_BASE, "quote/image.json"))
+        self.client.get(&self.build_uri(URI_BASE, "quote/image.json"))
             .send()?
             .model::<ImageResponse>()?
             .content()
     }
 
-    // GET: http://quotes.rest/quote/image.json?author=<author>&category=<category>
     pub fn image_query(&mut self, query: &Query) -> Result<Image> {
         if self.key.is_none() {
             return Err(Error::MethodUnavailable);
         }
 
-        Client::new().get(&apply_query(
+        self.client.get(&apply_query(
             self.build_uri(URI_BASE, "quote/image.json"),
             query,
         )).send()?.model::<ImageResponse>()?.content()
